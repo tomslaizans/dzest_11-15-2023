71aaf8ff65bf0a4618af28c90044c7585f9c96f0
tomslaizans <toms.laizans@grobinasskola.lv>
2023-11-15 10:49:36 +0200

    Matematik

---
+++ pirmais_uzd.py
@@ -0,0 +1,9 @@
+"""
+izveidot programmu, kura prasa lietotājam ievietot 2 skaitļus
+un aprēķina to summu izvadot terminālī
+"""
+
+a=int(input("ievadi 1. ciparu"))
+b=int(input("ievadi 2. ciparu"))
+c=a+b
+print(f"a+b={c}")
